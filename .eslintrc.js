33f1c02d73be7ced9901665907e880147baaa3da
smenezes <smenezes@cbu.edu>
2022-02-15 07:13:33 -0500

    Initial Commit

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    node: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['react', 'react-hooks', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+  },
+};
---
+++ README.md
@@ -0,0 +1,23 @@
+# react-template
+
+# Description
+
+Boiler plate code for a react project using parcel, eslint and prettier.
+
+## Installation
+```bash
+npm install
+```
+or
+```bash
+yarn install
+```
+
+## Usage
+```bash
+npm start
+```
+or
+```bash
+yarn start
+```
